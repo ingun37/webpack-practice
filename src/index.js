6e59f8a2643bb89b9f1774143db0a1351e3ee873
Ingun Jon <ingun@foxxing.com>
2020-09-16 16:40:32 +0900

    three test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { Object3D, Sprite } from "../node_modules/three/src/Three";
 import ClosetViewer from "./closet";
 import { getImage } from "./imgs";
 export { moduleFunc }
@@ -5,3 +6,8 @@ from "./module";
 export const closet = new ClosetViewer();
 
 console.log('apng base64: ', getImage('a.png'))
+
+const a = new Sprite()
+const b = new Object3D()
+b.add(a)
+console.log(b.children[0].material)
